7bf555bc693775141c097498ca0d81d201816ba9
Kris Sodroski <sodroski@bu.edu>
2023-07-20 17:51:21 -0400

    have project getting created

--- C#/Frontend/LinqlFrontendModelGenerator.cs
+++ C#/Frontend/LinqlFrontendModelGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Text.Json;
@@ -12,12 +13,33 @@ namespace Linql.ModelGenerator.Frontend
     {
         public LinqlFrontendModelGenerator() { }
 
-        public async Task Generate(string IntermediaryJson, string ProjectPath = "")
+        public async Task Generate(string IntermediaryJson, string ProjectPath = null)
         {
+            if(ProjectPath == null)
+            {
+                ProjectPath = Environment.CurrentDirectory;
+            }
+            IntermediaryModule module = null;
+
             using (Stream stream = this.GenerateStreamFromString(IntermediaryJson))
             {
-                IntermediaryModule module = await JsonSerializer.DeserializeAsync<IntermediaryModule>(stream);
+                module = await JsonSerializer.DeserializeAsync<IntermediaryModule>(stream);
             }
+
+            this.CreateProject(module, ProjectPath);
+        }
+
+        protected void CreateProject(IntermediaryModule Module, string ProjectPath)
+        {
+            Process process = new Process();
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("dotnet");
+            processStartInfo.Arguments = $"new classlib -o {Module.ModuleName}";
+            processStartInfo.WorkingDirectory = ProjectPath;
+            processStartInfo.CreateNoWindow = false;
+            process.StartInfo = processStartInfo;
+            process.Start();
+
+            File.Delete(Path.Combine(ProjectPath, Module.ModuleName, "Class1.cs"));
         }
 
         private Stream GenerateStreamFromString(string s)
