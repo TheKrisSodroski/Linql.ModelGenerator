3f9857a43d0de6b2b99f35545db8d3cab3272dab
Kris Sodroski <sodroski@bu.edu>
2023-07-23 21:02:38 -0400

    Add circular model frontend test

---
+++ C#/Test/Linql.ModelGenerator.Frontend.Test/LinqlModelGenerator_CircularModel.cs
@@ -0,0 +1,19 @@
+using Linql.ModelGenerator.Intermediary;
+using NUnit.Framework;
+using System.Text.Json;
+using Test.Module1;
+
+namespace Linql.ModelGenerator.Frontend.Test
+{
+    public class LinqlModelGenerator_CircularModel : BaseModelGeneratorTest
+    {
+
+        protected override string ModuleName { get; set; } = "Test.CircularModel";
+
+        [Test]
+        public async Task Generate()
+        {
+            this.Generator.Generate(this.ModuleJson);
+        }
+    }
+}
