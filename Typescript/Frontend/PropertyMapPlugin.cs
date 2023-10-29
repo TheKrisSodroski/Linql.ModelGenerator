6fd6df56ff3a41ac867b1c97439254d47f079575
Kris Sodroski <sodroski@bu.edu>
2023-10-29 17:35:33 -0400

    code cleanup and handle generic types

--- Typescript/Frontend/PropertyMapPlugin.cs
+++ Typescript/Frontend/PropertyMapPlugin.cs
@@ -8,6 +8,41 @@ namespace Linql.ModelGenerator.Typescript.Frontend
 {
     public class PropertyMapPlugin: TypescriptGeneratorPlugin
     {
+        protected string GetPropertyMapType(CoreType ParentType, CoreType PropertyType, LinqlModelGeneratorTypescriptFrontend Generator)
+        {
+            bool isPrimitive = PropertyType.IsPrimitive;
+            bool isListType = Generator.IsListType(PropertyType);
+            bool? isGeneric = ParentType.GenericArguments?.Any(s => s.TypeName == PropertyType.TypeName);
+            string type = "";
+
+            if (isListType)
+            {
+                type = this.GetPropertyMapType(ParentType, PropertyType.GenericArguments.First(), Generator);
+            }
+            else if (isGeneric == true)
+            {
+                type = "any";
+            }
+            else
+            {
+                type = Generator.BuildGenericType(PropertyType);
+            }
+
+            if (type == "any")
+            {
+                type = "Object";
+            }
+            else if (isPrimitive && type != "Date")
+            {
+                if (type != "Date")
+                {
+                    type = $"\"{type}\"";
+                }
+            }
+
+            return type;
+        }
+
         public override List<string> BeforePropertiesBuilt(CoreType Type, LinqlModelGeneratorTypescriptFrontend Generator)
         {
             List<string> pluginText = new List<string>();
@@ -23,35 +58,11 @@ namespace Linql.ModelGenerator.Typescript.Frontend
                 if (Type.IsClass && Type.Properties?.Count > 0)
                 {
                     string mapType = $"string, string | (new () => any)";
-                    pluginText.Add($"\tpublic static{overrideModifier}PropertyMap = new Map<{mapType}>([");
+                    pluginText.Add($"\tpublic static{overrideModifier}PropertyMap: Map<{mapType}> = new Map<{mapType}>([");
                     List<string> properties = new List<string>();
                     Type.Properties.ForEach(r =>
                     {
-                        bool isPrimitive = r.Type.IsPrimitive;
-                        bool isListType = Generator.IsListType(r.Type);
-                        string type = "";
-
-                        if (isListType)
-                        {
-                            type = Generator.BuildGenericType(r.Type.GenericArguments.First());
-                        }
-                        else
-                        {
-                            type = Generator.BuildGenericType(r.Type);
-                        }
-
-                        if (type == "any")
-                        {
-                            type = "Object";
-                        }
-                        else if (isPrimitive && type != "Date")
-                        {
-                            if (type != "Date")
-                            {
-                                type = $"\"{type}\"";
-                            }
-                        }
-
+                        string type = this.GetPropertyMapType(Type, r.Type, Generator);
                         properties.Add($"\t\t[\"{r.PropertyName}\", {type}]");
                     });
                     pluginText.Add(String.Join(",\n", properties));
