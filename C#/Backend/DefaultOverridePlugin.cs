7070943c5a152677f32d96c782adc801d5a4a4f3
inscriptrxbuildagents <inscriptrxbuildagents@inscriptrx.com>
2025-07-01 19:16:41 -0400

    Fix for linux extracting prereleases.

--- C#/Backend/DefaultOverridePlugin.cs
+++ C#/Backend/DefaultOverridePlugin.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Linql.ModelGenerator.CSharp.Backend
 {
     public class DefaultOverridePlugin : IModuleOverridePlugin
     {
+        protected Dictionary<string, string> DepVersion { get; set; }
+
         protected static List<Assembly> AssembliesToIgnore = new List<Assembly>()
             {
                 typeof(IComparable).Assembly,
@@ -39,14 +45,14 @@ namespace Linql.ModelGenerator.CSharp.Backend
 
         public bool IsValidType(Type Type)
         {
-            bool linqlBaseIgnore = !DefaultOverridePlugin.AssembliesToIgnore.Contains(Type.Assembly) 
+            bool linqlBaseIgnore = !DefaultOverridePlugin.AssembliesToIgnore.Contains(Type.Assembly)
                 && !DefaultOverridePlugin.NamespacesToIgnore.Contains(Type.Namespace)
                 && Type.GetCustomAttribute<LinqlGenIngore>() == null;
-            return linqlBaseIgnore 
+            return linqlBaseIgnore
                 && !IgnoreIfNameContains.Any(s => Type.Name.Contains(s));
         }
 
-        private bool isSealed(Type Type) 
+        private bool isSealed(Type Type)
         {
             return Type.IsSealed;
         }
@@ -63,16 +69,45 @@ namespace Linql.ModelGenerator.CSharp.Backend
 
         public string ModuleVersionOverride(Assembly Assembly)
         {
-            AssemblyInformationalVersionAttribute version = Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
-            string informationVersion = version.InformationalVersion;
-
-            if (informationVersion.Split('.').Count() > 3)
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
-                informationVersion = String.Join(".", informationVersion.Split('.').Take(3));
-            }
+                string assemblyName = Assembly.GetName().Name;
 
-            return informationVersion.Split('+')[0];
+                if (this.DepVersion == null)
+                {
+                    string dllLocation = Assembly.Location;
+                    string dllFolder = Path.GetDirectoryName(dllLocation);
+                    List<string> depFiles = Directory.GetFiles(dllFolder, "*.deps.json").ToList();
+                    string depFile = depFiles.FirstOrDefault();
+                    string depFileText = File.ReadAllText(depFile);
+                    var depFileObj = JsonSerializer.Deserialize<LinuxDepFile>(depFileText);
+
+                    this.DepVersion = depFileObj.libraries.Keys.ToDictionary(r => r.Split('/')[0], r => r.Split('/')[1]);
+                }
+
+                string versionNumber = this.DepVersion[assemblyName];
+
+                return versionNumber;
+            }
+            else
+            {
+                AssemblyInformationalVersionAttribute version = Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+                string informationVersion = version.InformationalVersion;
+
+                if (informationVersion.Split('.').Count() > 3)
+                {
+                    informationVersion = String.Join(".", informationVersion.Split('.').Take(3));
+                }
+
+                return informationVersion.Split('+')[0];
+            }
 
         }
     }
+
+    internal class LinuxDepFile
+    {
+        public Dictionary<string, Dictionary<string, object>> libraries { get; set; }
+    }
+
 }
